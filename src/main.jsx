e978cd439482d93bad650736f1f5d9ad3e8099a7
olcanarslan92-dot <olcanarslan92@gmail.com>
2025-08-05 02:51:14 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
-import './i18n';
+import './index.css'; // tailwind ya da global css
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
